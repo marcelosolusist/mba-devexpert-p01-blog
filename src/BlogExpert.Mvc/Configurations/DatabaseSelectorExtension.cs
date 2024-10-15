4f4853a9b80b0dafb5379b754e0e9d2c2abbd7e0
Marcelo Menezes <marcelo@solusist.com.br>
2024-10-15 07:26:30 -0300

    SqLite

--- src/BlogExpert.Mvc/Configurations/DatabaseSelectorExtension.cs
+++ src/BlogExpert.Mvc/Configurations/DatabaseSelectorExtension.cs
@@ -7,8 +7,16 @@ namespace BlogExpert.Mvc.Configurations
     {
         public static void AddDatabaseSelector(this WebApplicationBuilder builder)
         {
-            builder.Services.AddDbContext<BlogExpertDbContext>(options =>
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddDbContext<BlogExpertDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionLite")));
+            }
+            else
+            {
+                builder.Services.AddDbContext<BlogExpertDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            }
         }
     }
 }
